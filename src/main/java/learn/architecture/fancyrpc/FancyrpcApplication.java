08446a64e4e028b96678cbfa5f36aefdf3bc8db3
flow-lix <3185971499@qq.com>
2020-02-22 15:58:45 +0800

    init

---
+++ src/main/java/learn/architecture/fancyrpc/FancyrpcApplication.java
@@ -0,0 +1,13 @@
+package learn.architecture.fancyrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FancyrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FancyrpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/learn/architecture/fancyrpc/FancyrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.architecture.fancyrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FancyrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
